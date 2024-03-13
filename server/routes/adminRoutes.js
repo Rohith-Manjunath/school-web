e5fa19e98c039db1cbe8632a30e8382dce134384
Rohith-Manjunath <rohithmanjunath20@gmail.com>
2024-03-13 23:59:20 +0530

    feat : Admin can get all queries

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -1,5 +1,5 @@
 const express=require("express")
-const { postEvent, deleteEvent, getAllEvents, getUserDetails, updateUser, getAllUsers, deleteUser, updateEvent, getSingleEvent } = require("../controllers/AdminController")
+const { postEvent, deleteEvent, getAllEvents, getUserDetails, updateUser, getAllUsers, deleteUser, updateEvent, getSingleEvent, getAllAdmissionQueries } = require("../controllers/AdminController")
 const { isAuthenticatedUser, isAdmin } = require("../middlewares/isAuthenticated")
 const router=express.Router()
 
@@ -8,5 +8,6 @@ router.route("/event/:id").delete(isAuthenticatedUser,isAdmin, deleteEvent).put(
 router.route("/events").get(isAuthenticatedUser,isAdmin, getAllEvents);
 router.route("/user/:userId").get(isAuthenticatedUser,isAdmin, getUserDetails).put(isAuthenticatedUser,isAdmin, updateUser).delete(isAuthenticatedUser,isAdmin,deleteUser)
 router.route("/users").get(isAuthenticatedUser,isAdmin, getAllUsers)
+router.route("/queries").get(isAuthenticatedUser,isAdmin, getAllAdmissionQueries)
 
 module.exports = router
