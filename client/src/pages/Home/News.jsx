c9eb13df475135d30c18fe3172d0a28d6e7144a7
Rohith-Manjunath <rohithmanjunath20@gmail.com>
2024-05-13 20:41:03 +0530

    feat: Create reusable card component for news and events
    This commit introduces a new reusable card component that can be used for both news and events. By consolidating the card design and functionality into a single component, we reduce code duplication and improve maintainability.
    
    Changes:
    - Added a new component named NewsEventCard.
    - Implemented card layout and styling for both news and events.
    - Integrated functionality to handle delete and edit actions.
    
    This enhancement streamlines the codebase and ensures consistency in the presentation of news and events across the application.

--- client/src/pages/Home/News.jsx
+++ client/src/pages/Home/News.jsx
@@ -11,6 +11,7 @@ import { IoMdClose } from "react-icons/io";
 import Modal from "react-modal";
 import { useNewsUsersQuery } from "../../../Redux/authApi";
 import {LinearProgress, Stack } from "@mui/material";
+import NewsCard from "../../components/layouts/Cards/NewsCard";
 
 
 const News = () => {
@@ -123,37 +124,7 @@ const News = () => {
     <Slider {...settings} className="">
       {data?.news?.map((item) => {
         return (
-          <div
-            className="p-6 bg-white rounded-md space-y-4 text-textSecondary font-semibold tracking-wide h-[400px]"
-            key={item?.title}
-          >
-            <div>
-              <img
-                src={item?.avatar?.url}
-                alt=""
-                className="w-full h-[220px] rounded-lg "
-              />
-            </div>
-            <div className="space-y-4 relative">
-              <span className="text-[12px]">{item?.date} {item?.time} </span>
-              {isAdmin && (
-                <>
-                  <MdDelete
-                    title="Delete News"
-                    onClick={() => handleDelete(item?._id)}
-                    className="text-red-600 absolute top-[-3px] text-xl right-0 m-2 hover:cursor-pointer hover:scale-110 transition-all duration-200 hover:text-red-500"
-                  />
-                  <FaPen
-                  title="Edit News"
-                    className="text-blue-600 absolute top-0 text-md right-8 m-2 hover:cursor-pointer hover:scale-110 transition-all duration-200 hover:text-blue-500"
-                  />
-                </>
-              )}
-              <div>
-                <h4>{item?.title}</h4>
-              </div>
-            </div>
-          </div>
+        <NewsCard key={item?._id} handleDelete={handleDelete} item={item} isAdmin={isAdmin}/>
         );
       })}
     </Slider>
