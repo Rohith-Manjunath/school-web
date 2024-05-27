2c6b73234df12901938d99f5916046a52477fc86
Rohith-Manjunath <rohithmanjunath20@gmail.com>
2024-05-27 17:52:56 +0530

    Updated middleware to login so that only admin can login

--- server/routes/UserRoute.js
+++ server/routes/UserRoute.js
@@ -1,10 +1,10 @@
 const express=require('express');
 const { registerUser, loginUser, userQuery, admissionEnquiry, me, logout, parentsEnroll, scheduleVisit, getAllEventsForUsers, getAllNewsForUsers } = require('../controllers/UserController');
-const { isAuthenticatedUser } = require('../middlewares/isAuthenticated');
+const { isAuthenticatedUser, isAdmin } = require('../middlewares/isAuthenticated');
 const router=express.Router();
 
 router.route("/register").post(registerUser)
-router.route("/login").post(loginUser)
+router.route("/login").post(loginUser,isAdmin)
 router.route("/me").get(isAuthenticatedUser,me)
 router.route("/logout").post(isAuthenticatedUser,logout)
 router.route("/query").post(userQuery)
